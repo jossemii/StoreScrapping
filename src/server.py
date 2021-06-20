068838b94e3fc04ea1bfc13e393e7c46f84d3941
josemibnf <josemi.bnf@gmail.com>
2021-06-21 01:19:14 +0200

    .....

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+from time import monotonic
 from flask.json import jsonify
 from flask.wrappers import Request
 import scraping
@@ -12,8 +13,18 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = 'mongodb://' + MONGO_USER + ':' + MONGODB_PASSWORD \
                             + '@mongo:27017/' + MONGO_INITDB_DATABASE
 
+mongo = PyMongo(app)
+
 @app.route('/', methods=['GET', 'POST'])
-def hello():
-    return jsonify(scraping.main(request.get_json()['keywords']))
+def scrape():
+    data = scraping.main(request.get_json()['keywords'])
+    mongo.db.todos.insert_many(data)
+    return jsonify(data)
+
+@app.route('/', methods=['GET'])
+def get_all_data():
+    return jsonify(
+        mongo.db.find()
+    )
 
 app.run(host='0.0.0.0', port=8080, debug=False)
